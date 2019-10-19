f065126dd9d9fdcee09229e4e610e26fd802d961
camysandhu <fmsharmn@gmail.com>
2019-10-19 19:40:59 -0400

    Objects of Internet

--- C0764266_F2019_MID_TERM_MAD3004/main.swift
+++ C0764266_F2019_MID_TERM_MAD3004/main.swift
@@ -8,10 +8,13 @@
 
 import Foundation
 
-var b1 = Mobile(billId: 1, billDate: Date(), billType: billTypes.Hydro, totalBillAmount: 42.5, mobileManufacturerName: "Rogers", planName: "BigData", mobileNumber: 647123432, internetGbUsed: 23, minuteUsed: 12)
+var b1 = Mobile(billId: 1, billDate: Date(), billType: billTypes.Mobile, totalBillAmount: 42.5, mobileManufacturerName: "Rogers", planName: "BigData", mobileNumber: 647123432, internetGbUsed: 23, minuteUsed: 12)
 
 
-var b2 = Mobile(billId: 2, billDate: Date(), billType: billTypes.Internet, totalBillAmount: 65.4, mobileManufacturerName: "Fido", planName: "BigData and Calls", mobileNumber: 9471234545, internetGbUsed: 23, minuteUsed: 12)
+var b2 = Mobile(billId: 2, billDate: Date(), billType: billTypes.Mobile, totalBillAmount: 65.4, mobileManufacturerName: "Fido", planName: "BigData and Calls", mobileNumber: 9471234545, internetGbUsed: 23, minuteUsed: 12)
 
 //b1.printData()
 
+var b3 = Internet(billId: 1, billDate: Date(), billType: billTypes.Internet, totalBillAmount: 76.8, providerName: "Bell", internetGbUsed: 32)
+
+var b4 = Internet(billId: 2, billDate: Date(), billType: billTypes.Internet, totalBillAmount: 76.8, providerName: "Bell", internetGbUsed: 32)
