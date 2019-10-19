41addbcff652be5636829a163ed78bea9e6cd1cd
camysandhu <fmsharmn@gmail.com>
2019-10-19 18:23:45 -0400

    Internet(func Display)

--- C0764266_F2019_MID_TERM_MAD3004/Internet.swift
+++ C0764266_F2019_MID_TERM_MAD3004/Internet.swift
@@ -13,7 +13,15 @@ class Internet: Bill
     var providerName: String
     var internetGbUsed: Int
     
-    init(billId: Int, billDate: String, billType: billTypes, totalBillAmount: Double)
+    init(billId: Int, billDate: String, billType: billTypes, totalBillAmount: Double, providerName: String, internetGbUsed: Int)
+    {
+        self.providerName = providerName
+        self.internetGbUsed = internetGbUsed
+        
+        super.init(billId: billId, billDate: billDate, billType: billType, totalBillAmount: totalBillAmount)
+    }
+    
+    func printData()
     {
         
     }
